abd0f324816732b9a60d33252a67b1cc81c31997
oiTaka <monkypunk6@gmail.com>
2022-05-04 14:51:22 +0000

    Update

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,3 +1,26 @@
 class Public::UsersController < ApplicationController
-  
+
+  def show
+    @user = User.find(params[:id])
+    @blogs = @user.blogs.all
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).parmit(:name, :profile_img, :self_introduction)
+  end
+
 end
